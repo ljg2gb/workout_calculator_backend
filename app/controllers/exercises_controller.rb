17dc2e00023403e19b0605f92cfed465fe007cf4
Lydia Gregory <ljg2gb@virginia.edu>
2020-04-28 13:49:05 -0600

    adds exercise controller actions

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,2 +1,41 @@
 class ExercisesController < ApplicationController
+    def index
+        @exercises = Exercise.all
+        render :json @exercises
+    end
+
+    def show
+        @exercise = Exercise.find(params[:id])
+        render json: @exercise
+    end
+
+    def create
+        @exercise = Exercise.create(
+            duration: params[:duration],
+            type: params[:type],
+            intensity: params[:intensity]
+        )
+        render json: @exercise
+
+    end
+
+    def update
+        @exercise = Exercise.update(
+            duration: params[:duration],
+            type: params[:type],
+            intensity: params[:intensity]
+        )
+        render json: @exercise
+    end
+
+    def destroy
+        @exercise = Exercise.find(params[:id])
+        @exercise.destroy
+        render json: {message: "Exercise successfully deleted"}
+    end
+
+    def create_rest_hrs(create)
+        @exercise = Exercise.find()
+        
+    end
 end
