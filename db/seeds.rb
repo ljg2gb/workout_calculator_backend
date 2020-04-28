f999d36746f132118a2d0e51a16579e7e95ee259
Lydia Gregory <ljg2gb@virginia.edu>
2020-04-28 14:13:58 -0600

    adds exercise seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,9 @@
 
 Exercise.destroy_all
 
-exercise1 = Exercise.create(duration: 60, type: cardio, intensity: low, current_hr:)
+exercise1 = Exercise.create(duration: 60, type: 4, intensity: 1, current_hr:0)
+exercise2 = Exercise.create(duration: 480, type: 3, intensity: 2, current_hr:0)
+exercise3 = Exercise.create(duration: 20, type: 3, intensity: 3, current_hr:0)
+exercise4 = Exercise.create(duration: 45, type: 4, intensity: 2, current_hr:0)
+exercise5 = Exercise.create(duration: 35, type: 4, intensity: 3, current_hr:0)
+exercise6 = Exercise.create(duration: 720, type: 4, intensity: 2, current_hr:0)
